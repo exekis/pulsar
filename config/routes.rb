f5678534a1135a74fc01ccb8d4ea5498c5d2f5b3
exekis <kiarashsotoudeh@gmail.com>
2025-05-15 23:36:25 -0400

    add initial routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Devise routes for User auth (if you’re using Devise)
+  devise_for :users
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # Root path
+  root 'polls#index'
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  # Our CRUD routes
+  resources :polls
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Only create action for votes
+  resources :votes, only: :create
 end
